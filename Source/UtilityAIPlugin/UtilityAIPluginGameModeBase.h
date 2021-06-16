3f861065f42f2fe9dc9617a40e9ad511ffe05d4e
Bohdon Sayre <bohdon@gmail.com>
2021-06-15 22:00:43 -0400

    add project and UtilityAI plugin

---
+++ Source/UtilityAIPlugin/UtilityAIPluginGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UtilityAIPluginGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UTILITYAIPLUGIN_API AUtilityAIPluginGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
